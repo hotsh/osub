8178ea459827d06f726120ba9de5c4ed33cc4590
wilkie <wilkie05@gmail.com>
2011-03-14 00:23:30 -0400

    Added subscription verification.
    This is useful when people might attempt to subscribe you for things you do not want. No religious pamplets please.

--- lib/osub/subscriber.rb
+++ lib/osub/subscriber.rb
@@ -8,9 +8,8 @@ module OSub
     attr_reader :callback_url
     attr_reader :topic_url
 
-    def initialize(callback_url, topic_url, secret = nil, token = nil)
-      token = "" if token == nil
-      @token = token.to_s
+    def initialize(callback_url, topic_url, secret = nil)
+      @tokens = []
 
       secret = "" if secret == nil
       @secret = secret.to_s
@@ -20,27 +19,40 @@ module OSub
     end
 
     # Subscribe to the topic through the given hub.
-    def subscribe(hub_url = @topic_url)
-      change_subscription(:subscribe, hub_url)
+    def subscribe(hub_url = @topic_url, token = nil)
+      if token != nil
+        @tokens << token.to_s
+      end
+      change_subscription(:subscribe, hub_url, token)
     end
 
     # Unsubscribe to the topic through the given hub.
-    def unsubscribe(hub_url = @topic_url)
-      change_subscription(:unsubscribe, hub_url)
+    def unsubscribe(hub_url = @topic_url, token = nil)
+      if token != nil
+        @tokens << token.to_s
+      end
+      change_subscription(:unsubscribe, hub_url, token)
     end
 
-    def change_subscription(mode, hub_url)
+    def change_subscription(mode, hub_url, token)
       res = Net::HTTP.post_form(URI.parse(hub_url),
                                 { 'hub.mode' => mode.to_s,
                                   'hub.callback' => @callback_url,
                                   'hub.verify' => 'async',
-                                  'hub.verify_token' => @token,
+                                  'hub.verify_token' => token,
                                   'hub.lease_seconds' => '',
                                   'hub.secret' => @secret,
                                   'hub.topic' => @topic_url})
     end
 
-    def verify(body, signature)
+    def verify_subscription(token)
+      result = @tokens.index(token) != nil
+      @tokens.delete(token)
+
+      result
+    end
+
+    def verify_content(body, signature)
       hmac = HMAC::SHA1.hexdigest(@secret, body)
       check = "sha1=" + hmac
       check == signature
@@ -52,13 +64,13 @@ module OSub
   end
 
   module Subscriptions
-    def Subscriptions.new(callback_url, topic_url, secret = nil, token = nil)
+    def Subscriptions.new(callback_url, topic_url, secret = nil)
       if not defined?(@@instances)
         @@instances = {}
       end
 
       if @@instances[topic_url] == nil
-        sub = OSub::Subscription.new(callback_url, topic_url, secret, token)
+        sub = OSub::Subscription.new(callback_url, topic_url, secret)
         @@instances[topic_url] = sub
       end
 
